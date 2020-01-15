41ee929f57df399006b6a218f898b2a4f74bb2a6
miebacode <2994333015@qq.com>
2020-01-15 14:47:21 +0800

    更新代码

--- pages/trashClass/trashClass.js
+++ pages/trashClass/trashClass.js
@@ -4,12 +4,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentLeft: 0, //左侧选中的下标
-    selectId: "item0",  //当前显示的元素id
-    scrollTop: 0, //到顶部的距离
-    select_index: 0,
-    scroll_height: 0,
-    heightArr:[],
+    // tabbar
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换
+    currentTab: 0,
+    scrollLeft: 0,
     trashTypes:[{
       id:1,
       name:"厨余垃圾",
@@ -42,60 +42,20 @@ Page({
     }]
 
   },
-  //选择项目左侧点击事件 currentLeft：控制左侧选中样式  selectId：设置右侧应显示在顶部的id
-  proItemTap:function(e) {
-    this.setData({
-      currentLeft: e.currentTarget.dataset.pos,
-      selectId: "item" + e.currentTarget.dataset.pos
-    })
-  },
-  //计算右侧每一个分类的高度，在数据请求成功后请求即可
-  selectHeight() {
-    let that = this;
-    this.heightArr = [];
-    let h = 0;
-    const query = wx.createSelectorQuery();
-    query.selectAll('.pro-box').boundingClientRect()
-    query.exec(function (res) {
-      res[0].forEach((item) => {
-        h += item.height;
-        that.heightArr.push(h);
-      })
-      console.log(that.heightArr);
-    })
-  },
-
-  //监听scroll-view的滚动事件
-  scrollEvent:function(event) {
-    if (this.heightArr.length == 0) {
-      return;
-    }
-    let scrollTop = event.detail.scrollTop;
-    let current = this.data.currentLeft;
-    if (scrollTop >= this.distance) { //页面向上滑动
-      //如果右侧当前可视区域最底部到顶部的距离 超过 当前列表选中项距顶部的高度（且没有下标越界），则更新左侧选中项
-      if (current + 1 < this.heightArr.length && scrollTop >= this.heightArr[current]) {
-        this.setData({
-          currentLeft: current + 1
-        })
-      }
-    } else { //页面向下滑动
-      //如果右侧当前可视区域最顶部到顶部的距离 小于 当前列表选中的项距顶部的高度，则更新左侧选中项
-      if (current - 1 >= 0 && scrollTop < this.heightArr[current - 1]) {
-        this.setData({
-          currentLeft: current - 1
-        })
-      }
-    }
-    //更新到顶部的距离
-    this.distance = scrollTop;
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.selectHeight();
+    var that = this;
+    /*** 获取系统信息*/
+    wx.getSystemInfo({
+       success: function (res){ 
+         that.setData({
+            winWidth: res.windowWidth,
+             winHeight: res.windowHeight 
+             }); 
+             } 
+             });
   },
 
   /**
@@ -145,5 +105,33 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /*** 滑动切换tab*/
+  bindChange: function (e) {
+    var that = this;
+     that.setData({ 
+       currentTab: e.detail.current 
+       });
+       // 内容与tabbar的联动//这里的 2 75 是根据顶部tabbar的个数来决定的，我定义的是5个，2是索引，也就是说超过三页才会改变
+       if (e.detail.current > 2)
+        {
+          var a = e.detail.currentvar 
+          var query = wx.createSelectorQuery()
+          query.select('.scrollBox').boundingClientRect(function (res) {
+            that.setData({scrollLeft: (a - 2) * 75})})
+            query.selectViewport().scrollOffset()
+            query.exec(function (res) {})} 
+            else {
+              var a = e.detail.current
+              this.setData({scrollLeft: 0})}
+               },
+ /*** 点击tab切换*/
+  switchNav: function (e) {
+        var that = this;console.log(e.target)
+        if (this.data.currentTab === e.target.dataset.current) 
+            {return false;} 
+        else {
+            that.setData({currentTab: e.target.dataset.current})}},
+  
 })
